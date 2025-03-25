f51896be5871eef1304c40e5ac698e5783b942b6
mytkom <marek.mytkowski.mm@gmail.com>
2025-03-25 10:38:06 +0100

    adjust all sweep configs to new code

--- configuration/lr_and_bs/run_sweeps.sh
+++ configuration/lr_and_bs/run_sweeps.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
 
-SWEEP_SUBDIR=data_augmentation
+SWEEP_SUBDIR=lr_and_bs
 python3 sweep.py --config configuration/$SWEEP_SUBDIR/config.json --model.architecture=CNN --cnn.architecture=Classic --project-dir=${SWEEP_SUBDIR}_Classic --sweep.name=Classic
 python3 sweep.py --config configuration/$SWEEP_SUBDIR/config.json --model.architecture=CNN --cnn.architecture=ResNet --project-dir=${SWEEP_SUBDIR}_ResNet --sweep.name=ResNet
 python3 sweep.py --config configuration/$SWEEP_SUBDIR/config.json --model.architecture=CNN --cnn.architecture=VGGlike --project-dir=${SWEEP_SUBDIR}_VGGlike --sweep.name=VGGlike
--- configuration/many_hyperparameters_random/run_sweeps copy.sh
+++ configuration/many_hyperparameters_random/run_sweeps copy.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/bash
 
-SWEEP_SUBDIR=data_augmentation
+SWEEP_SUBDIR=many_hyperparameters_random
 python3 sweep.py --config configuration/$SWEEP_SUBDIR/config.json --model.architecture=CNN --cnn.architecture=Classic --project-dir=${SWEEP_SUBDIR}_Classic --sweep.name=Classic
 python3 sweep.py --config configuration/$SWEEP_SUBDIR/config.json --model.architecture=CNN --cnn.architecture=ResNet --project-dir=${SWEEP_SUBDIR}_ResNet --sweep.name=ResNet
 python3 sweep.py --config configuration/$SWEEP_SUBDIR/config.json --model.architecture=CNN --cnn.architecture=VGGlike --project-dir=${SWEEP_SUBDIR}_VGGlike --sweep.name=VGGlike
-python3 sweep.py --config configuration/$SWEEP_SUBDIR/config.json --model.architecture=ViT --project-dir=${SWEEP_SUBDIR}_ViT
