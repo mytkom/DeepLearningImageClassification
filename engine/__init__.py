89df24fd0233a053cb53ab319cf2e6b12021c9c1
Julia <jdudzinska625@gmail.com>
2025-03-30 11:49:28 +0200

    RIP 23hr sweep

--- engine/__init__.py
+++ engine/__init__.py
@@ -1,5 +1,5 @@
 from .few_shot_engine import FewShotEngine
-from .sweep_engine import SweepEngine
+from .sweep_engine import SweepEngine, FewShotSweepEngine
 from .train_engine import Engine
 
 
@@ -10,5 +10,7 @@ def build_engine(engine_name: str):
         return SweepEngine
     elif engine_name == "few_shot_engine":
         return FewShotEngine
+    elif engine_name == "few_shot_sweep_engine":
+        return FewShotSweepEngine
     else:
         raise ValueError(f"Unknown engine: {engine_name}")
